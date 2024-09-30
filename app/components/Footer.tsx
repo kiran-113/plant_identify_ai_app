2faf5269396d2bb65bcb6d68829f7549a64b9080
harsha <nakirikantikiran@outlook.com>
2024-09-30 20:55:45 +0530

    first commit

---
+++ app/components/Footer.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="mt-16 text-center text-white">
+      <p>
+        &copy; Plant Identifier. Made by {"NKV Harsha"}
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
